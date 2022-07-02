734022e651661c79c87846b1566b34f290a6a942
Ethan Cooper <cooper@Ethans-MacBook-Air.local>
2022-07-02 17:20:56 -0500

    adding new codes

--- code_samples/012.test/variables_values_types/ninja-1/e-1/main.go
+++ code_samples/012.test/variables_values_types/ninja-1/e-1/main.go
@@ -6,10 +6,34 @@ var x int =42
 var y string = "James Bond"
 var z bool = true
 
+var t int
+var v string
+
+type ethan int
+var bx ethan
+
 func main(){
+
+	bx = 100
+	fmt.Println(bx)
+
+	x = int(bx)
+	fmt.Println(x)
+
+	z := 50 
+	fmt.Println(z)
 
 	fmt.Println(x, y, z)
 	fmt.Println(x)
 	fmt.Println(y)
 	fmt.Println(z)
+
+	fmt.Println("===================")
+
+	fmt.Println(t)
+	fmt.Println(v)
+
+	fmt.Println("===================")
+	s := fmt.Sprintf("%v\t%v\t%v\t",x,y,z)
+	fmt.Println(s)
 }
