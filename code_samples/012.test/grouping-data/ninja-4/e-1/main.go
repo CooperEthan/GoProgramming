b410fb5dfbf31a0bcf6147b219cf749d0b3d57f1
Ethan Cooper <cooper@Ethans-MacBook-Air.local>
2022-07-04 17:21:26 -0500

    grouping data

---
+++ code_samples/012.test/grouping-data/ninja-4/e-1/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+
+func main() {
+
+	var x [5]int
+
+	x[0] = 1
+	x[1] = 2
+	x[2] = 3
+	x[3] = 4
+	x[4] = 5
+
+	for i, xi := range x {
+		fmt.Println(i, xi)
+	} 
+	// =============================================
+
+	y := [] int{42,43,44,45,46,47,48,49,50,51}
+	fmt.Println(y[:5])
+	fmt.Println(y[5:])
+	fmt.Println(y[2:7])
+	fmt.Println(y[1:6])
+	
+	//==============================================
+
+}
---
+++ code_samples/012.test/grouping-data/ninja-4/e-4/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+
+func main() {
+
+	a := []string{"James","Bond","Shaken, not stirred"}
+	b := []string{"Mis", "Moneypenny", "Helloooo", "James"}
+
+	c := [][]string{a,b}
+	fmt.Println(c)
+
+	for i, d := range c {
+		fmt.Println("record: ", i)
+		for j,v := range d {
+			fmt.Printf("\tindex position: %v \t value: \t %v \n", j , v)
+		}
+	}
+
+}
