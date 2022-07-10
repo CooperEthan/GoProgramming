2b6ae90965f6c0a9e5c743a0055296b0dd30d82c
Ethan Cooper <cooper@Ethans-MacBook-Air.local>
2022-07-09 20:59:08 -0500

    pointers

---
+++ code_samples/012.test/pointers/ninja-7/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type person struct{
+	name string
+}
+func changeMe(p *person) {
+	p.name = "MisMoneypenny"
+	(*p).name = "James Bond"
+
+}
+
+func main() {
+
+	a := 42
+	fmt.Println("value is=",a)
+	fmt.Println("Adress of the value=",&a)
+	fmt.Println("====================")
+
+	p1 := person {
+		name: "James Bond",
+	}
+	changeMe(&p1)
+	fmt.Println(*&p1)
+	// changeMe(&p1)
+	fmt.Println(&p1)
+}
---
+++ code_samples/012.test/pointers/pointers-3_method_sets/main.go
@@ -0,0 +1,44 @@
+// method sets
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type circle struct{
+	r float64
+}
+
+type square struct{
+	r float64
+}
+
+type shape interface{
+	area()float64
+}
+func (c *circle) area()float64 {
+	return math.Pi*c.r*c.r
+}
+func (s *square) area()float64 {
+	return s.r*s.r
+}
+
+func info(s shape) {
+	fmt.Println(s.area())
+}
+
+func main() {
+
+	s := square{
+		r: 5,
+	}
+	c := circle {
+		r: 5,
+	}
+	info(&c)
+	fmt.Printf("%T\n",c)
+	info(&s)
+	fmt.Printf("%T\n",s)
+
+}
