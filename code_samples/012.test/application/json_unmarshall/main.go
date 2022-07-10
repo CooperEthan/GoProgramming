734def32d7e0c097b626d55c9a7a36b2560db3ac
Ethan Cooper <cooper@Ethans-MacBook-Air.local>
2022-07-09 21:38:58 -0500

    marshall & unmarshall

---
+++ code_samples/012.test/application/json_unmarshall/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+	
+type person struct {
+	First string `json:"First"`
+	Last  string `json:"Last"`
+	Age   int    `json:"Age"`
+}
+
+func main(){
+
+	s := `[{"First":"James","Last":"Bond","Age":32},{"First":"Miss","Last":"Moneypenny","Age":27}]`
+	bs := []byte(s)
+	fmt.Printf("%T\n",bs)
+	fmt.Printf("%T\n",s)
+
+	// people := []person{}
+	var people []person
+
+	err := json.Unmarshal(bs, &people)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	fmt.Println("all of the data:", people)
+
+	for i,v := range people {
+		fmt.Println("\nPERSON NUMNER:",i)
+		fmt.Println(v.First,v.Last,v.Age)
+	}
+
+}
