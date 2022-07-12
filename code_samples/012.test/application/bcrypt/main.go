75945a949fdab8f4b8eaf15346239479f2cd962b
Ethan Cooper <cooper@Ethans-MacBook-Air.local>
2022-07-11 19:51:45 -0500

    application

---
+++ code_samples/012.test/application/bcrypt/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+
+func main() {
+
+	s := `password123`
+	bs, err := bcrypt.GenerateFromPassword([]byte(s), bcrypt.MinCost)
+	if err != nil {
+		fmt.Println("Error",err)
+	}
+	fmt.Println(s)
+	fmt.Println(bs)
+	loginpassword := `password123`
+	err = bcrypt.CompareHashAndPassword(bs, []byte(loginpassword))
+	if err != nil {
+		fmt.Println("You cant login")
+		return
+	}
+	fmt.Println("you are logged in!!!")
+}
---
+++ code_samples/012.test/application/writer_interface/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"sort"
+)
+
+func main() {
+
+	fmt.Fprintln(os.Stdout, "Hello")
+	io.WriteString(os.Stdout,"Hello")
+
+	xi := []int{4,7,3,42,99,18,16,56,12}
+	xs := []string{"James", "Q", "M", "Moneypenny", "Dr. No"}
+
+	sort.Ints(xi)
+	fmt.Println(xi)
+	sort.Strings(xs)
+	fmt.Println(xs)
+
+}
