985e712712b2e888bc502a92999edc5aa6162276
Ethan Cooper <cooper@Ethans-MacBook-Air.local>
2022-07-23 21:00:50 -0500

    testing, benchmarking

---
+++ code_samples/012.test/testing_benchmarking/benchmark/00-live/saying/main_test.go
@@ -0,0 +1,25 @@
+package saying
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	s := Greet("James")
+	if s != "James" {
+		// t.Error("got",s,"expected","James")
+		t.Error("got",s,"expected","James")
+	}
+}
+
+func ExampleGreet() {
+	fmt.Println(Greet("James"))
+	
+}
+
+func BenchmarkGreet(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Greet("James")
+	}
+}
