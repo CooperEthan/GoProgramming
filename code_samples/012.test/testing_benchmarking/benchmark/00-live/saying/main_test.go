c28a837c54dd832a8482d1a3f8dc13c3a68586be
Ethan Cooper <cooper@Ethans-MacBook-Air.local>
2022-07-23 21:01:21 -0500

    testing

--- code_samples/012.test/testing_benchmarking/benchmark/00-live/saying/main_test.go
+++ code_samples/012.test/testing_benchmarking/benchmark/00-live/saying/main_test.go
@@ -9,13 +9,13 @@ func TestGreet(t *testing.T) {
 	s := Greet("James")
 	if s != "James" {
 		// t.Error("got",s,"expected","James")
-		t.Error("got",s,"expected","James")
+		t.Error("got", s, "expected", "James")
 	}
 }
 
 func ExampleGreet() {
 	fmt.Println(Greet("James"))
-	
+
 }
 
 func BenchmarkGreet(b *testing.B) {
